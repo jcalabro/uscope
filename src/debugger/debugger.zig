a5e380da9ddcb185ee7037784fda71a04b200f19
jcalabro <jamesrcalabro@gmail.com>
2025-02-02 07:42:05 -0500

    add a check against zero-length var_name

--- src/debugger/debugger.zig
+++ src/debugger/debugger.zig
@@ -2180,7 +2180,7 @@ fn DebuggerType(comptime AdapterType: anytype) type {
                 if (self.data.target.?.strings.get(params.variable.name)) |n| break :blk n;
                 return;
             };
-            if (!strings.eql(params.expression, var_name)) return;
+            if (!strings.eql(params.expression, var_name) or var_name.len == 0) return;
 
             const var_platform_data = switch (builtin.target.os.tag) {
                 .linux => if (params.variable.platform_data.location_expression) |loc|
